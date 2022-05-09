5a10e0c9153243b5f2bb2182a110805c227ec181
loulou8011984D <louptaxi@gmail.com>
2022-05-09 16:04:20 +0200

    color

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+const wilders = ["db", "nico","clochette","lilou"];
+    console.log(chalk.blue(`hello ${wilders[0]}`));
+    console.log(chalk.red(`hello ${wilders[1]}`));
+    console.log(chalk.yellow(`hello ${wilders[2]}`));
+    console.log(chalk.green(`hello ${wilders[3]}`));
+
+
